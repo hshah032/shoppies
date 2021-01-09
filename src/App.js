18723819d6f166fc05d0cb133d4ac570388c3c04
Dragon Duck <dragonduck@Dragons-MacBook-Pro.local>
2021-01-09 09:24:22 -0700

    completed draft, now onto SHOPPIES

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddToFavorites from './components/AddToFavorites';
+import RemoveFavourites from './components/RemoveFavourites';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
   const [favourites, setFavourites] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  
   //   {
   //   "Title": "Star Wars: Episode IV - A New Hope",
   //           "Year": "1977",
@@ -34,7 +36,7 @@ const App = () => {
 
   // ]);
 
-  const getMovieRequest = async () => {
+  const getMovieRequest = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=2db75176`;
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -44,15 +46,38 @@ const App = () => {
     }
   };
 
-  const addFavouriteMovie = (movie) => {
-    const newFavouriteList =  [...favourites, movie];
-    setFavourites(newFavouriteList);
-  }
-
   useEffect(()=>{
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  useEffect(()=>{
+    const movieFavourites = JSON.parse(
+      localStorage.getItem('react-movie-app-favourites')
+    );
+
+    setFavourites(movieFavourites);
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favourites', JSON.stringify(items))
+  };
+
+
+  const addFavouriteMovie = (movie) => {
+    const newFavouriteList =  [...favourites, movie];
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  }
+
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite)=>favourite.imdbID !== movie.imdbID
+    );
+
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
+  
   return (
     <div className ='container-fluid shoppies'>
       <div className = 'row d-flex align-items-center mt-4 mb-4'>
@@ -71,7 +96,13 @@ const App = () => {
         <MovieListHeading heading='Favourites' />
       </div>
       <div className = 'row'>
-        <MovieList movies={favourites} favouriteComponent={AddToFavorites} />
+        <MovieList 
+          movies={favourites} 
+          handleFavouriteClick={removeFavouriteMovie}
+          favouriteComponent={RemoveFavourites} 
+
+          
+          />
       </div>
     </div>
   );
