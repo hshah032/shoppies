8393e0bc8b00ae607147c08cca05b555104b0719
Dragon Duck <dragonduck@Dragons-MacBook-Pro.local>
2021-01-17 19:33:46 -0700

    published

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=2db75176`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=2db75176`;
     const response = await fetch(url);
     const responseJson = await response.json();
 
